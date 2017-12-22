34faeda914b53e227033a851145cc2e78eaf0816
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 20:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,25 +2,8 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
-  // this.veto = function(){
-  //   return 'No, I must disagree'
-  // }
-  // this.approve = function(){
-  //   return 'You can do that!'
-  // }
-  // this.doCharity = function(){
-  //   return "I like to help people."
-  // }
-  //
-  // this.releasePressStatement = function(){
-  //   return "You will see great things from Scuber."
-  // }
-  //
-  // this.sayHi = function(){
-  //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  // }
 }
+
 BoardMember.prototype.veto = function() {
   return  "No, I must disagree"
 }
@@ -40,3 +23,21 @@ BoardMember.prototype.releasePressStatement = function(){
 BoardMember.prototype.sayHi = function() {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
+
+// this.veto = function(){
+//   return 'No, I must disagree'
+// }
+// this.approve = function(){
+//   return 'You can do that!'
+// }
+// this.doCharity = function(){
+//   return "I like to help people."
+// }
+//
+// this.releasePressStatement = function(){
+//   return "You will see great things from Scuber."
+// }
+//
+// this.sayHi = function(){
+//   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+// }
